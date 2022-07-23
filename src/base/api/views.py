022b6871d4e60efc5197dc9c22828061aff59fc9
EzraEzeiru <ezeiru.ezra@gmail.com>
2022-07-23 23:46:36 +0100

    Adding Api Feature

---
+++ src/base/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import Room
+from serializers import RoomSerializer
+
+@api_view(['GET'])
+def get_routes(request):
+    routes = [
+        "GET /api/",
+        "GET /api/rooms",
+        "GET /api/room/:id"
+    ]
+    return Response(routes)
+
+@api_view(['GET'])
+def get_rooms(request):
+    rooms = Rooms.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
